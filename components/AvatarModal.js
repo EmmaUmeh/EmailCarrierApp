d61308b1cfa47e5c8bd24a6190b86db6d2bfda68
Emmanuel Umeh <emmachinemerem04@gmail.com>
2023-10-27 09:57:40 -0700

    Added_Loginpage

---
+++ components/AvatarModal.js
@@ -0,0 +1,19 @@
+
+
+const AvatarModal = () => {
+    return (
+        <>
+
+            <div className="fixed right-20 top-12 z-50 flex">
+                <div className="w-30 p-8 bg-white border border-secondary rounded-lg">
+                    <div className="flex flex-col">
+                    <span>Add Account</span>
+                    <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AvatarModal;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,19 @@
 "use client";
 
-import React from "react"
+import React, { useState } from "react"
 import { BiSearch } from "react-icons/bi"
+import Avatar from "./Avatar";
+import { GrNotification } from "react-icons/gr"
+import AvatarModal from "./AvatarModal";
 
 export default function Navbar() {
+
+    const [avatarmodal, setAvatarModal] = useState(false)
+
+
+    const handleAvatarModal = () => {
+        setAvatarModal(!avatarmodal)
+    }
     return(
         <>
             <div className="w-full bg-white">
@@ -11,9 +21,22 @@ export default function Navbar() {
                         <span className="font-lg">Inbox</span>
                         
                         <div className="flex">
-                            <BiSearch size={20} color="rgb(163 163 163)" className="absolute top-6 ml-1"/>
-                            <input placeholder="Search" className="border-2 rounded-full border-[#E0E0E0] px-2 pl-5 bg-[#E0E0E0] focus:outline-none focus:border-[#E0E0E0] focus:border-[#E0E0E0]"/>
+                            <BiSearch size={20} color="rgb(163 163 163)" className="absolute top-8 ml-1"/>
+                            <input placeholder="Search" className="border-2 rounded-full border-[#E0E0E0] px-2 pl-5 pr-20 bg-[#E0E0E0] focus:outline-none focus:border-[#E0E0E0] focus:border-[#E0E0E0]"/>
                         </div>
+
+                        <div className="flex items-center gap-3">
+                            <GrNotification size={20}/>
+                            <Avatar width={40} height={40} onClick={handleAvatarModal}/>
+                            <div className="flex flex-col text-sm">
+                                    <h5>Emmanuel Umeh</h5>
+                                    <span>emmanuel@gmail.com</span>
+                            </div>
+                        </div>
+
+                     
+                            {avatarmodal && <AvatarModal /> }
+                        
                 </div>
             </div>
         </>
